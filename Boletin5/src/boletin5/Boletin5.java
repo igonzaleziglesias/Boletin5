757a43ac9d7bad39443831a2ce6f105f9054f122
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2018-10-23 11:10:53 +0200

    boletin 5

---
+++ Boletin5/src/boletin5/Boletin5.java
@@ -0,0 +1,37 @@
+package boletin5;
+
+import java.util.Scanner;
+
+public class Boletin5 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        Conta contaOrigen = new Conta("Pedro Picapiedra", "1", 1000);
+        Conta contaDestino = new Conta();
+        contaDestino.setNome("Mario Moreno 'Cantinflas'");
+        contaDestino.setNumConta("2");
+        contaDestino.setSaldo(2000);
+        contaOrigen.visualizar();
+        contaDestino.visualizar();
+        contaOrigen.ingreso(3000);
+        contaDestino.reintegro(500);
+        contaOrigen.visualizar();
+        contaDestino.visualizar();
+        
+        System.out.println("Indique la cantidad a transferir: ");
+        double cant=ler.nextDouble();
+        
+        if (contaDestino.transferencia(contaOrigen, cant)==true){
+            System.out.println("\nTransferencia realizada");
+        } else {
+            System.out.println("ERROR DE TRANSFERENCIA");
+        }
+        contaOrigen.visualizar();
+        contaDestino.visualizar();
+          
+        
+    
+    
+    }
+
+}
